90ebee34323bdd21e576f127f83f9c3d226d3f37
David Crossley <dcrossley@indexgeo.com.au>
2018-06-04 18:20:23 +1000

    Use getopts

--- lint-raml/lint-raml-cop.sh
+++ lint-raml/lint-raml-cop.sh
@@ -1,10 +1,71 @@
 #!/usr/bin/env bash
 
 help_msg="Some assistance is at https://dev.folio.org/guides/raml-cop"
+default_ramls_dir="ramls"
 
-# The directory to start searching for RAML files.
-# Relative to the root of the repository.
-ramls_dir="ramls"
+usage() {
+  cat << EOM
+
+Usage:
+
+$(basename $0) REPO_NAME
+$(basename $0) [-h] [-b BASE_DIR] [-d RAMLS_DIR] REPO_NAME
+
+Investigate RAML and schema files.
+
+Required:
+  REPO_NAME      The repository name, e.g.: mod-notes
+
+Optional:
+  -h             Display this help and exit.
+  -b BASE_DIR    The pathname of the directory holding the git checkouts.
+                 Default assumes next to "folio-tools", so: ../
+  -d RAMLS_DIR   The directory within the project to commence searching for RAML files.
+                 Relative to the root of the repository.
+                 Default: ${default_ramls_dir}
+EOM
+}
+
+base_dir=../
+repo_name=""
+ramls_dir="${default_ramls_dir}"
+
+while getopts ":hb:d:" opt; do
+  case ${opt} in
+    h)
+      usage
+      exit 0
+      ;;
+    b)
+      base_dir=$OPTARG
+      ;;
+    d)
+      ramls_dir=$OPTARG
+      ;;
+    :)
+      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+      usage
+      exit 1
+      ;;
+    \?)
+      echo "Invalid Option: -$OPTARG" 1>&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+if [ $# -eq 0 ]; then
+  echo "REPO_NAME is required."
+  usage
+  exit 0
+fi
+
+repo_name="${1}"
+
+echo "repo_name=${repo_name}"
+exit 0
 
 # Space-separated list of sub-directory paths that need to be avoided.
 prune_dirs="raml-util"
