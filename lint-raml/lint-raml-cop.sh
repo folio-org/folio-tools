50e79a24f0e9773ba8eb867ef276851dfc3fd134
David Crossley <david@indexdata.com>
2018-06-05 17:21:10 +1000

    Enable option -a AVOID_DIRS
    And set known defaults for certain repos

--- lint-raml/lint-raml-cop.sh
+++ lint-raml/lint-raml-cop.sh
@@ -2,6 +2,7 @@
 
 help_msg="Some assistance is at https://dev.folio.org/guides/raml-cop"
 default_ramls_dir="ramls"
+default_avoid_dirs="raml-util rtypes traits types"
 
 usage() {
   cat << EOM
@@ -9,7 +10,7 @@ usage() {
 Usage:
 
 ${0##*/} REPO_NAME
-${0##*/} [-h] [-b BASE_DIR] [-d RAMLS_DIR] REPO_NAME
+${0##*/} [-h] [-b BASE_DIR] [-d RAMLS_DIR] [-a "AVOID_DIRS"] REPO_NAME
 
 Investigate RAML and schema files.
 
@@ -23,14 +24,19 @@ Optional:
   -d RAMLS_DIR   The directory within the project to commence searching for RAML files.
                  Relative to the root of the repository.
                  Default: ${default_ramls_dir}
+  -a AVOID_DIRS  Space-separated list of sub-directory paths that need to be avoided.
+
+  Note that for options -d and -a there are pre-configured values,
+  so just specifying the required REPO_NAME should be sufficient. 
 EOM
 }
 
 base_dir="../.."
 repo_name=""
-ramls_dir="${default_ramls_dir}"
+ramls_dir=""
+avoid_dirs=""
 
-while getopts ":hb:d:" opt; do
+while getopts ":hb:d:a:" opt; do
   case ${opt} in
     h)
       usage
@@ -41,6 +47,9 @@ while getopts ":hb:d:" opt; do
       ;;
     d)
       ramls_dir=$OPTARG
+      ;;
+    a)
+      avoid_dirs=$OPTARG
       ;;
     :)
       echo "Invalid Option: -$OPTARG requires an argument" 1>&2
@@ -64,8 +73,28 @@ fi
 
 repo_name="${1}"
 
-# Space-separated list of sub-directory paths that need to be avoided.
-prune_dirs="raml-util"
+if [ -z "$ramls_dir" ]; then
+  case ${repo_name} in
+    okapi)
+      ramls_dir="okapi-core/src/main/raml"
+      ;;
+    raml-module-builder)
+      ramls_dir="domain-models-api-interfaces/ramls"
+      ;;
+    *)
+      ramls_dir="${default_ramls_dir}"
+  esac
+fi
+
+if [ -z "$avoid_dirs" ]; then
+  case ${repo_name} in
+    mod-data-loader)
+      avoid_dirs="${default_avoid_dirs} inventory"
+      ;;
+    *)
+      avoid_dirs="${default_avoid_dirs}"
+  esac
+fi
 
 script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cmd="${script_home}/node_modules/.bin/raml-cop"
@@ -87,7 +116,8 @@ fi
 
 cd "${repo_dir}" || exit
 
-prune_string=$(printf " -path ${ramls_dir}/%s -o" ${prune_dirs})
+prune_string=$(printf " -path ${ramls_dir}/%s -o" ${avoid_dirs})
+echo "prune_string=${prune_string}"
 raml_files=($(find ${ramls_dir} \( ${prune_string% -o} \) -prune -o -name "*.raml" -print))
 
 if [[ ${#raml_files[@]} -eq 0 ]]; then
