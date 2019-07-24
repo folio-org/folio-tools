4207670fac21ddf723bfb0c577a83f018ef6ea8e
Ian Hardy <ian.c.hardy@gmail.com>
2019-07-24 10:49:46 -0400

    use find instead of cp to avoid error if no files present

--- kubernetes-utilities/folio-ansible-runner/entrypoint.sh
+++ kubernetes-utilities/folio-ansible-runner/entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 git clone $ANSIBLE_REPO_URL
 cd ./$ANSIBLE_REPO
-cp /etc/ansible/playbooks/* ./
+#cp /etc/ansible/playbooks/* ./
+find /etc/ansible/playbooks -type f -exec cp {} ./ \;
 
 exec "$@"
 
