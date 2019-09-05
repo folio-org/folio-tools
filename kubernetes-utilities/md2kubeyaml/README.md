1d0eff308a90a0ce402e58dad2d094401780ef95
Ian Hardy <ian.c.hardy@gmail.com>
2019-09-05 14:31:44 -0400

    first pass FOLIO-2219

---
+++ kubernetes-utilities/md2kubeyaml/README.md
@@ -0,0 +1,63 @@
+# md2kubeyaml
+This utility takes a module descriptor as input and provides example Kubernetes deployment YAML from the descriptor.
+
+## Examples
+Generate Kubernetes Deployment YAML from a file
+```bash
+python3 md2kubernetes -f ModuleDescriptor.json > deployment-yaml.yml
+```
+Take input from a web resource
+```bash
+python3 md2kubernetes -u \
+ http://folio-registry.aws.indexdata.com/_/proxy/modules/mod-login-6.0.0 \
+ > deployment-yaml.yml
+```
+Run from the docker container, Take input from stdin
+```bash
+cat ModuleDescriptor.json | docker run -i \
+  folioci/md2kubeyaml > deployment-yaml
+```
+Note that when using the Docker container, to take input from stdin, you must use the `-i` interactive flag. Input from stdin or url is preferable over reading a file on disk when using the Docker container because the container will not have access to your filesystem.
+
+Specify a kubernetes secret to load environment variables from, and remove DB connection information:
+```bash
+cat ModuleDescriptor.json | docker run -i \
+  folioci/md2kubeyaml --env-from-secret db-connect \
+  --remove-db-env > deployment-yaml
+```
+Its common practice to keep DB connection information in a Kubernetes secret. Use the above options to omit DB connection information from the deployment YAML, and to include an existing Kubernetes secret instead.
+
+## Usage
+```
+usage: md2kubeyaml.py [-h] [-f FILE] [-u URL] [-r] [-s] [-e ENV_FROM_SECRET]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FILE, --file FILE  Module Descriptor file to parse
+  -u URL, --url URL     URL for Module Descriptor to parse
+  -r, --remove-db-env   Remove database configuration variables(use with
+                        --env-from-secret option to provide db connection info
+                        as secret)
+  -s, --include-service
+                        Include a service in deployment YAML
+  -e ENV_FROM_SECRET, --env-from-secret ENV_FROM_SECRET
+                        Name of kubernetes secret to load env from
+```
+
+## Install
+### Docker
+Run from the docker container, for example:
+```bash
+cat ModuleDescriptor.json | docker run -i folioci/md2kubeyaml 
+```
+
+### Python
+You can also run the python script. Use python3's `venv` module to set up a virtual environemnt if you do not want to install dependencies on the system python:
+```bash
+sudo apt install -y python3-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+# make sure the script runs
+python md2kubeyaml.py --help
+```
---
+++ kubernetes-utilities/md2kubeyaml/md2kubeyaml.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+import argparse
+import json
+import requests
+import sys
+from jinja2 import Environment, FileSystemLoader, Template
+
+def main():
+    args = parse_command_line_args()
+    if args.url:
+        md = parse_from_url(args.url)
+    elif args.file:
+        md = parse_from_file(args.file)
+    elif not sys.stdin.isatty():
+        md = parse_from_stdin()
+    else:
+        sys.exit("no input specified")
+        
+    if args.remove_db_env is True:
+        md = filter_db_secrets(md)
+    deployment_yaml = render_template(md, 'folio-default',
+                                      'module-deployment.yml.j2')
+    if args.include_service == True:
+        service_yaml = render_template(md, 'folio-default',
+                                       'module-service.yml.j2')
+        config_yaml = '\n'.join([deployment_yaml, service_yaml])
+    else:
+        config_yaml = deployment_yaml
+    sys.stdout.write(config_yaml)
+
+def parse_command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='Module Descriptor file to parse',
+                        default=False, required=False)
+    parser.add_argument('-u', '--url', help='URL for Module Descriptor to parse',
+                        default=False, required=False)
+    parser.add_argument('-r', '--remove-db-env',help='Remove database configuration variables' 
+                        '(use with --env-from-secret option to provide db connection info as secret)',
+                        action='store_true', required=False)
+    parser.add_argument('-s', '--include-service', help='Include a service in deployment YAML',
+                        action='store_true', required=False)
+    parser.add_argument('-e', '--env-from-secret', help='Name of kubernetes secret to load env from',
+                        default=False, required=False)
+
+    args = parser.parse_args()
+
+    return args
+
+def parse_from_file(md_file):
+    try:
+        with open(md_file, 'r') as fh:
+            md = json.load(fh)
+    except FileNotFoundError as e:
+        sys.exit(': '.join([e.strerror, e.filename]) )
+    except json.decoder.JSONDecodeError as e:
+        sys.exit("{} is not valid JSON".format(md_file))
+    return md
+
+def parse_from_url(url):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError as e:
+        sys.exit("Could not connect to " + e.request.url)
+    try:
+        md = r.json()
+    except json.decoder.JSONDecodeError:
+        sys.exit("Could not decode JSON")
+    return md
+
+def parse_from_stdin():
+    data = sys.stdin.read()
+    try:
+        md = json.loads(data)
+    except json.decoder.JSONDecodeError:
+        sys.exit("Could not decode JSON")
+    
+    return md
+
+def filter_db_secrets(md):
+    if 'env' in md['launchDescriptor']:
+        env = [var for var in md['launchDescriptor']['env'] 
+               if var['name'][:3] != "DB_"] 
+        md['launchDescriptor']['env'] = env
+    return md
+
+def render_template(md, namespace, template,
+                    env_from_secret=False):
+    j2loader = FileSystemLoader('templates')
+    j2env = Environment(loader=j2loader)
+    t = j2env.get_template(template)
+ 
+    result = t.render(md=md,
+                      namespace=namespace,
+                      env_from_secret=env_from_secret)
+
+    return result
+
+if __name__ == "__main__":
+    main()
