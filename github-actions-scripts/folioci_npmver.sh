f50f7516452b0b41bb5a5b2938a2f1b592e40904
John Malconian <malc@indexdata.com>
2021-03-02 17:37:57 -0500

    add script for github actions

---
+++ github-actions-scripts/folioci_npmver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# for testing only
+#BUILD_NUMBER=123
+
+if [ ! -e package.json ]; then
+   echo "package.json file not found"
+   exit 2
+fi
+
+hasjq=`which jq`
+
+if [ -z "$hasjq" ]; then
+  echo "jq not found"
+  exit 2
+fi
+
+cur_ver=`cat package.json | jq -r .version`
+
+maj_ver=$(echo $cur_ver | awk -F '.' '{ print $1 }')
+min_ver=$(echo $cur_ver | awk -F '.' '{ print $2 }')
+patch_ver=$(echo $cur_ver | awk -F '.' '{ print $3 }')
+
+if [ "$patch_ver" == "0" ]; then
+  patch_ver=1
+fi
+
+new_cur_ver=${maj_ver}.${min_ver}.${patch_ver}
+
+# add 000+Jenkins BUILD_NUMBER to current patch version
+
+new_snap_ver=${new_cur_ver}000${GITHUB_RUN_NUMBER}
+echo "$new_snap_ver"
+
