f3013f94c5b1d9ba608282fa3cf7707c4810f224
Zak Burke <zburke@ebsco.com>
2023-11-02 10:27:33 -0400

    FOLIO-3922 handle updated Wiki page (#302)
    * The FOLIO responsibility matrix URL changed and its HTML was utterly
      structured. Handle both.
    * Update outdated dependencies.
    * Migrate from CJS to ESM. It is The Way.
    
    Refs FOLIO-3922

--- parse-matrix/parse-matrix.js
+++ parse-matrix/parse-matrix.js
@@ -1,5 +1,5 @@
-const axios = require("axios");
-const { parse } = require('node-html-parser');
+import axios from 'axios';
+import { parse } from 'node-html-parser';
 
 /**
  * Create tickets for all jira projects associated with packages in
@@ -16,58 +16,35 @@ class ParseMatrix {
   {
     const modules = {};
     const matrix = (await axios.get(matrixUrl)).data;
+    // const matrix = fs.readFileSync('/Users/zburke/Downloads/matrix.html', { encoding: 'UTF-8' });
 
     const userFromTd = (td) => {
-      return td.querySelector ? td.querySelector('a')?.getAttribute('data-username') : null;
+      let name = td.querySelector ? td.querySelector('a')?.getAttribute('data-username')?.trim() : null;
+      if (name) {
+        name = name.replace(/\s+/g, ' ');
+      }
+      return name;
     }
 
-    const teams = parse(matrix).querySelectorAll('.confluenceTable tbody tr');
-
-    // pluck the first row, pretending it's a <thead> effectively,
-    // so it can be used as a template for parsing other rows
-    // which may have missing team or product-owner cells because
-    // a td above spans multiple rows.
-    const ths = Array.from(teams.shift().querySelectorAll('td'));
-
-    let pteam = { team: '', po: '', tl: '', github: '', jira: '' };
-    teams.forEach((tr, i) => {
+    const container = parse(matrix).querySelector('.confluenceTable');
+    const rows = Array.from(container.querySelectorAll('tbody tr'));
+    rows.forEach((tr, i) => {
       const tds = Array.from(tr.querySelectorAll('td'));
-
-      // jigger tds so it always resembles ths, i.e. so there is always a 1::1
-      // correspondence between the th and the td. if we have a table like this:
-      //
-      // | TEAM | PO | TL | junk | REPO | JIRA |
-      // | Aaaa | Aa | Aa | junk | Aaaa | Aaaa |
-      // |      | Bb | Bb | junk | Bbbb | Bbbb |
-      // | Cccc | Cc | Cc | junk | Cccc | Cccc |
-      // |      |    | Dd | junk | Dddd | Dddd |
-      //
-      // then the A-row and C-row have the same number of cols, but the B-row
-      // is short by one and the D-row is short by two because of the cells
-      // above that span multiple rows.
-
-      while (tds.length < ths.length) {
-        tds.unshift({ text: '' });
-      }
-
       const team = { team: '', po: '', tl: '', github: '', jira: '' };
       // I don't really know what kind of data structure `tds` is.
       // iterating with (td, j) works just fine, but trying to access tds[j] fails.
       tds.forEach((td, j) => {
-        if (j == 0) team.team = td.text || pteam.team;
-        if (j == 1) team.po = userFromTd(td) || pteam.po;
-        if (j == 2) team.tl = userFromTd(td) || pteam.tl;
-        if (j == 4) team.github = td.text;
-        if (j == 5) team.jira = td.text;
+        if (j == 0) team.jira = td.text?.trim();
+        if (j == 1) team.team = (td.text?.trim()).split(/\n/)[0].trim();
+        if (j == 2) team.po = userFromTd(td);
+        if (j == 3) team.tl = userFromTd(td);
+        // there is no rule 4
+        if (j == 5) team.github = td.text?.trim();
       });
 
       if (team.github) {
         modules[team.github] = team;
       }
-
-      // cache current team to deal with rowspans, i.e. rows that inherit
-      // their first few fields from a parent row.
-      pteam = team;
     });
 
     return modules;
@@ -76,7 +53,7 @@ class ParseMatrix {
   async main()
   {
     try {
-      const matrix = await this.getMatrix('https://wiki.folio.org/display/REL/Team+vs+module+responsibility+matrix');
+      const matrix = await this.getMatrix('https://wiki.folio.org/pages/viewpage.action?pageId=14463134');
       console.log(JSON.stringify(matrix, null, 2));
     }
     catch(e) {
